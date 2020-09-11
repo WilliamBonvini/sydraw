ee7d55691ad753f6afec1118676482cf91debd72
William Bonvini <10526185@polimi.it>
2020-09-11 12:52:03 +0200

    first commit

---
+++ coxswain.py
@@ -0,0 +1,18 @@
+'''
+simply a class that handles in what folder do what
+'''
+class Coxswain:
+
+    def __init__(self, homeDir):
+        self._homeDir = homeDir
+
+    def setTestDir(self,testDir):
+        self._testDir
+
+    def setTrainDir(self,trainDir):
+        self._trainDir
+
+
+
+
+
